acd18818a844a70dfdb70d7159775f08ec5aaa83
valera <67824716+OnishkovValera@users.noreply.github.com>
2024-11-29 17:36:04 +0300

    Initalize project with spring initializer

---
+++ src/main/java/org/onishkoff/itmo/IS1/Is1Application.java
@@ -0,0 +1,13 @@
+package org.onishkoff.itmo.IS1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Is1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Is1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/onishkoff/itmo/IS1/Is1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.onishkoff.itmo.IS1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Is1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
